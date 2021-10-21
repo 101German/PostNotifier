e343bf31c8a3d3f238914788a672c134fe634238
101German <gervika@list.ru>
2021-10-21 18:39:10 +0300

    Remove unuseful code

--- PostNotifier/Consumers/PostCreatingConsumer.cs
+++ PostNotifier/Consumers/PostCreatingConsumer.cs
@@ -1,13 +1,10 @@
 ﻿using MassTransit;
-using MimeKit;
 using PoemPost.Data.DTO;
 using PostNotifier.Contracts;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PostNotifier.Consumers
@@ -45,35 +42,6 @@ namespace PostNotifier.Consumers
                     await smtp.SendMailAsync(mailMessage);
                 }
             }
-
-
-
-           
-         
- 
-
-
-            //var subscriptions = await _subscriptionRepository.FindAsync(context.Message.AuthorId);
-
-            //var emailMessage = new MimeMessage();
-
-            //emailMessage.From.Add(new MailboxAddress("Администрация сайта", "herman.shvayukou@innowise-group.com"));
-            //emailMessage.To.Add(new MailboxAddress("name","geras210e@gmail.com"));
-            //emailMessage.Subject = "Hi";
-            //emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            //{
-            //    Text = context.Message.PoemText
-            //};
-
-            //using (var client = new SmtpClient())
-            //{
-            //    await client.ConnectAsync("smtp.gmail.com", 587,false);
-            //    await client.AuthenticateAsync("herman.shvayukou@innowise-group.com", "gERA5127716");
-            //    await client.SendAsync(emailMessage);
-
-            //    await client.DisconnectAsync(true);
-            //}
-
         }
     }
 }
