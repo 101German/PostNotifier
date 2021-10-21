c15c4abf9f1055e480e6f8924843f53814096750
101German <gervika@list.ru>
2021-10-21 15:08:47 +0300

    Add subscription and unsubscription consumers

---
+++ PostNotifier/Consumers/SubscriptionConsumer.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MassTransit;
+using Models;
+using PostNotifier.Contracts;
+using PostNotifier.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostNotifier.Consumers
+{
+    public class SubscriptionConsumer : IConsumer<UserForSubscription>
+    {
+        private readonly ISubscriptionRepository _subscriptionRepository;
+        private readonly IMapper _mapper;
+
+        public SubscriptionConsumer(ISubscriptionRepository subscriptionRepository, IMapper mapper)
+        {
+            _subscriptionRepository = subscriptionRepository;
+            _mapper = mapper;
+        }
+        public async Task Consume(ConsumeContext<UserForSubscription> context)
+        {
+            var subscriptiopn = _mapper.Map<Subscription>(context.Message);
+            var existSubscription = await _subscriptionRepository.GetAsync(subscriptiopn.AuthorId, subscriptiopn.Email);
+
+            if (existSubscription == null)
+            {
+                await _subscriptionRepository.InsertAsync(subscriptiopn);
+            }
+        }
+    }
+}
---
+++ PostNotifier/Consumers/UnsubscriptionConsumer.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Models;
+using PostNotifier.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostNotifier.Consumers
+{
+    public class UnsubscriptionConsumer : IConsumer<UserForUnsubscription>
+    {
+        private readonly ISubscriptionRepository _subscriptionRepository;
+
+        public UnsubscriptionConsumer(ISubscriptionRepository subscriptionRepository)
+        {
+            _subscriptionRepository = subscriptionRepository;
+        }
+
+        public async Task Consume(ConsumeContext<UserForUnsubscription> context)
+        {
+            var existSubscription = await _subscriptionRepository.GetAsync(context.Message.AuthorId, context.Message.Email);
+
+            if (existSubscription != null)
+            {
+              await  _subscriptionRepository.RemoveAsync(existSubscription.AuthorId,existSubscription.Email);
+            }
+        }
+    }
+}
