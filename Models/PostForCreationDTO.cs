d290824611f8332cc7b2b508fe23515e87498772
101German <gervika@list.ru>
2021-10-21 18:34:14 +0300

    Add DTO models

---
+++ Models/PostForCreationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PoemPost.Data.DTO
+{
+    public class PostForCreationDTO
+    {
+        public string Title { get; set; }
+        public string PoemText { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
