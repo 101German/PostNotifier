0a0bc09d5e5a19e891149f3123082a7faf4b8621
101German <gervika@list.ru>
2021-10-21 15:11:21 +0300

    Add subscription profile

---
+++ PostNotifier/Mapping/SubscriptionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Models;
+using PostNotifier.Models;
+
+namespace PostNotifier.Mapping
+{
+    public class SubscriptionProfile : Profile
+    {
+        public SubscriptionProfile()
+        {
+            CreateMap<UserForSubscription, Subscription>();
+        }
+    }
+}
