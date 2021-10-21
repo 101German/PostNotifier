c201e6f2732ef1ec845182d07bf0bc762df8dc7c
101German <gervika@list.ru>
2021-10-21 15:10:02 +0300

    Add subscription controller

---
+++ PostNotifier/Controllers/SubscriptionController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostNotifier.Contracts;
+using PostNotifier.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostNotifier.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly ISubscriptionRepository _subscriptionRepository;
+
+        public SubscriptionController(ISubscriptionRepository subscriptionRepository)
+        {
+            _subscriptionRepository = subscriptionRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Subscription subscription)
+        {
+            await _subscriptionRepository.InsertAsync(subscription);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Remove(int authorId,string email)
+        {
+            await _subscriptionRepository.RemoveAsync(authorId, email);
+
+            return NoContent();
+        }
+    }
+}
