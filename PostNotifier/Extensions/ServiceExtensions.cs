da3ebbbe68cb9c3f9cd84d966532e57663bac756
101German <gervika@list.ru>
2021-10-21 15:10:45 +0300

    Add services extensions

---
+++ PostNotifier/Extensions/ServiceExtensions.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PostNotifier.Consumers;
+using PostNotifier.Contracts;
+using PostNotifier.Mapping;
+using PostNotifier.Models;
+using PostNotifier.Repositories;
+
+namespace PostNotifier.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureDatabaseSettings(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.Configure<SubscriptionsDatabaseSettings>(
+        Configuration.GetSection(nameof(SubscriptionsDatabaseSettings)));
+
+            services.AddSingleton<ISubscriptionDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<SubscriptionsDatabaseSettings>>().Value);
+        }
+
+        public static void ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton<ISubscriptionRepository, SubscriptionRepository>();
+        }
+
+        public static void ConfigureAutoMapper(this IServiceCollection services)
+        {
+            var mapConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new SubscriptionProfile());
+
+            });
+
+            IMapper mapper = mapConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+
+        public static void ConfigureMassTransit(this IServiceCollection services)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<SubscriptionConsumer>();
+                x.AddConsumer<UnsubscriptionConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.ReceiveEndpoint("subscription-queue", e =>
+                    {
+                        e.ConfigureConsumer<SubscriptionConsumer>(context);
+                    });
+
+                    cfg.ReceiveEndpoint("unsubscription-queue", e =>
+                    {
+                        e.ConfigureConsumer<UnsubscriptionConsumer>(context);
+                    });
+                });
+            });
+
+            services.AddMassTransitHostedService();
+        }
+    }
+}
--- PostNotifier/Startup.cs
+++ PostNotifier/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PostNotifier.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,10 @@ namespace PostNotifier
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.ConfigureDatabaseSettings(Configuration);
+            services.ConfigureRepositories();
+            services.ConfigureAutoMapper();
+            services.ConfigureMassTransit();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
