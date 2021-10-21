2f99cd10d40c3c0d269f482dd68645d7ba9b1c70
101German <gervika@list.ru>
2021-10-21 15:09:43 +0300

    Implement interfaces

---
+++ PostNotifier/Models/SubscriptionsDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using PostNotifier.Contracts;
+
+namespace PostNotifier.Models
+{
+    public class SubscriptionsDatabaseSettings : ISubscriptionDatabaseSettings
+    {
+
+        public string SubscriptionsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
