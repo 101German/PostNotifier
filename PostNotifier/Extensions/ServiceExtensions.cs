1ee6d9079eb166afdcdbf20a0c754ad5e7a63c22
101German <gervika@list.ru>
2021-11-26 16:54:51 +0300

    Update service extensions

--- PostNotifier/Extensions/ServiceExtensions.cs
+++ PostNotifier/Extensions/ServiceExtensions.cs
@@ -1,11 +1,9 @@
-﻿using AutoMapper;
-using MassTransit;
+﻿using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using PostNotifier.Consumers;
 using PostNotifier.Contracts;
-using PostNotifier.Mapping;
 using PostNotifier.Models;
 using PostNotifier.Repositories;
 
@@ -27,38 +25,14 @@ namespace PostNotifier.Extensions
             services.AddSingleton<ISubscriptionRepository, SubscriptionRepository>();
         }
 
-        public static void ConfigureAutoMapper(this IServiceCollection services)
-        {
-            var mapConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new SubscriptionProfile());
-
-            });
-
-            IMapper mapper = mapConfig.CreateMapper();
-            services.AddSingleton(mapper);
-        }
-
         public static void ConfigureMassTransit(this IServiceCollection services)
         {
             services.AddMassTransit(x =>
             {
-                x.AddConsumer<SubscriptionConsumer>();
-                x.AddConsumer<UnsubscriptionConsumer>();
                 x.AddConsumer<PostCreatingConsumer>();
 
                 x.UsingRabbitMq((context, cfg) =>
                 {
-                    cfg.ReceiveEndpoint("subscription-queue", e =>
-                    {
-                        e.ConfigureConsumer<SubscriptionConsumer>(context);
-                    });
-
-                    cfg.ReceiveEndpoint("unsubscription-queue", e =>
-                    {
-                        e.ConfigureConsumer<UnsubscriptionConsumer>(context);
-                    });
-
                     cfg.ReceiveEndpoint("newpost-queue", e =>
                     {
                         e.ConfigureConsumer<PostCreatingConsumer>(context);
