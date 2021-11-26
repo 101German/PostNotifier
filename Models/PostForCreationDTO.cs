a2621c8ac3e86538435542aba4d7514cac34862e
101German <gervika@list.ru>
2021-11-26 16:53:40 +0300

    Update models

--- Models/PostForCreationDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PoemPost.Data.DTO
-{
-    public class PostForCreationDTO
-    {
-        public string Title { get; set; }
-        public string PoemText { get; set; }
-        public int AuthorId { get; set; }
-    }
-}
---
+++ Models/UserForNotify.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class UsersForNotify
+    {
+        public List<string> UsersEmails { get; set; }
+        public string AuthorName { get; set; }
+        public string PoemName { get; set; }
+    }
+}
